a065a511952aac8e2e0cd012ec2325f1e8c95225
Tdogb <tdogbeard@gmail.com>
2020-12-25 19:52:59 -0500

    working

--- Dynamics AI/main.py
+++ Dynamics AI/main.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 def plant_dynamics(x, u):
     A = np.array([[1.5,0],[0,1.2]])
@@ -11,6 +13,11 @@ def plant_dynamics_damaged(x, u):
     B = np.array([1,1]).T
     return A@x + B@u
 
+def plant_dynamics_simple(x,u):
+    return 1*x+4*u
+
+def plant_dynamics_damaged_simple(x,u):
+    return 1*x+3*u+math.tanh(x*0.003)*10# #+ u*5*math.sin(x*0.2)
 #This will be the controller for the normal plant dynamics
 def plant_inverse_dynamics(x_new, x):
     return 0
@@ -21,51 +28,47 @@ def xr(t):
 
 def main():
     tf.random.set_seed(42)
-    duration = 10
-    t = 0
-    threshold = 0.0001
-    x = np.array([0,0]).T
-    error = 0
-    u = np.array([0,0]).T
+    samples = 20
+    #Input is u and x, train with u's and x's go to actual values
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units=1, input_shape=(1,)),
-        # tf.keras.layers.Dense(1, activation='linear'),
+        tf.keras.layers.Dense(units=2, input_shape=(2,)),
+        tf.keras.layers.Dense(units=40*(2+1), activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear'),
     ])
-    x = np.array([[0]])
-    y = np.array([[0]])
-    y_expected = np.array([[0]])
-    for t in range(0, 10): #Arbitrary amount of points in my simple example
-        input = np.array([t,t]) #Our input to plant for sample gathering is arbitrarily [t,t].T
-        x = np.vstack((x,input[0]))
-        plant_actual = plant_dynamics_damaged([y[t].T, 0], input.T).T
-        plant_expected = plant_dynamics([y[t].T, 0], input.T).T
-        y = np.vstack((y, plant_actual[0]))
-        y_expected = np.vstack((y_expected, plant_expected[0]))
-    # print(y)
-    # print(y_expected)
-    model.compile(optimizer='adam',
-              loss='mse',
+    x = 0
+    error = 0
+    error_array = []
+    x_array = []
+    u_array = []
+    x_p1_array = [] #actual dynamics array
+    x_p1_e_array = [] #expected dynamics array
+    t_array = range(0,samples)
+    for t in t_array: #Arbitrary amount of points in my simple example
+        u = t*0.4
+        actual_dynamics = plant_dynamics_damaged_simple(x,u)
+        expected_dynamics = plant_dynamics_simple(x,u)
+        error = actual_dynamics-expected_dynamics
+        error_array.append(error)
+        x_array.append(x)
+        u_array.append(u)
+        x_p1_array.append(actual_dynamics)
+        x_p1_e_array.append(expected_dynamics)
+        x = actual_dynamics
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+    model.compile(optimizer=optimizer,
+              loss='mae',
               metrics=['accuracy'])
     print(model.summary())
-    model.fit(y, y_expected, epochs=50)
+    print(np.array([error_array,u_array]).T)
+    model.fit(np.array([error_array,u_array]).T, np.array(error_array), epochs=500)
     print("Prediction")
-    model.predict(y, verbose=1)
-
-    # while True:
-    #     trajectory_point = xr(t)
-    #     if error > threshold:
-
-    #     else:
-    #         u = plant_inverse_dynamics(trajectory_point, x)
-    #     x_new = plant_dynamics(x, u)
-    #     error = x_new - trajectory_point
-    #     x = x_new
-
-# def function():
-#     uk = 0
-#     sum = 0
-#     for k in range(t,t+T):
-#         sum += information_function(uk) + lamb*safety_function()
-
+    prediction = model.predict(np.array([error_array,u_array]).T, verbose=1)
+    # prediction = np.ndarray.flatten(prediction) + x_p1_e_array
+    print(prediction)
+    t_array_2 = range(samples, samples+30)
+    prediction2 = model.predict(np.array([error_array,u_array]).T, verbose=1)
+    plt.plot(t_array, error_array)
+    plt.plot(t_array, prediction)
+    plt.show()
 if __name__ == "__main__":
     main()
